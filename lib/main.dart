a2a7f73ce7be9ef94d72cc5dc7e472e466580b2c
Myron Hicks <hicks.myronj@gmail.com>
2024-07-11 19:10:27 -0500

    flutter sdk setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,21 @@ import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:provider/provider.dart';
+//firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  /**
+   * This allows firebase to use platform channels to call native code
+   * Without it, firebase won't work correctly.
+   */
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (ctx) => CharacterStore(),
